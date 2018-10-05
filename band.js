eee489b4860e6c7ed0cbc29664f01884d3ebe7de
Thomas Martin <tmartin.dev101010@gmail.com>
2018-10-04 18:19:01 -0600

    Bandss in town is working correctly.

---
+++ band.js
@@ -0,0 +1,34 @@
+// Importing rwquest 
+var request = require("request");
+
+// Function to process the Bands in Town request
+function processBandsInTown() {
+    let artist = process.argv[3];
+    let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    // Get the data from Bands In Town
+    let bandInfo = request(queryURL, (error, response, body) => {
+        console.log("Into the function");
+        if (!error && response.statusCode === 200) {
+            // Make the data a regular JSON object
+            let parsedData = JSON.parse(body);
+            for( let event = 0; event < parsedData.length; ++event) {
+                // console.log();
+                console.log(parsedData[event].venue.name);
+    
+                let location = parsedData[event].venue.city + ", ";
+                if (parsedData[event].venue.region != "") {
+                    location += parsedData[event].venue.region + ", ";
+                }
+                location += parsedData[event].venue.country;
+    
+                console.log(location);
+                console.log(parsedData[event].datetime);
+            }
+
+            
+        };
+    });
+
+}
+module.exports = processBandsInTown;
--- liri.js
+++ liri.js
@@ -8,43 +8,12 @@ if (dotenv.error) {
 var keys = require("./keys.js");
 console.log(keys.spotify);
 
-// Importing rwquest 
-var request = require("request");
-
-// Function to process the Bands in Town request
-function processBandsInTown () {
-    let artist = process.argv[3];
-    let queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-    // Get the data from Bands In Town
-    let bandInfo = request( queryURL, (error, response, body) => {
-        console.log("Into the function");
-        if(!error && response.statusCode === 200) {
-            // console.log(JSON.parse(body)[0].venue);
-            let parsedData = JSON.parse(body);
-            // for( event in parsedData) {
-            //     console.log(event);
-            // }
-            console.log(parsedData[0].venue);
-            console.log(parsedData.length);
-            parsedData.forEach((element) => {
-                console.log(element.venue);
-            })
-        }
-
-        // console.log(error);
-        // console.log(response.statusCode);
-        // console.log(body);
-    }); 
-
-
-}
-
+var bandVenues = require("./band.js");
 
 // Switch statement to arse the arguments
 switch (process.argv[2]) {
     case "concert-this":
-        processBandsInTown();
+        bandVenues();
         break;
     case "spotify-this-song":
         break;
