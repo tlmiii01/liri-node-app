e5f0ee351eb7077a51a5a54c998eeba2cb8edadd
Thomas Martin <tmartin.dev101010@gmail.com>
2018-10-06 08:48:07 -0600

    OMDB output is working correctly.

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 var bandVenues = require("./band.js");
 var spotifySongData = require("./spotify.js");
+var movieData = require("./omdb.js");
 
 // Switch statement to arse the arguments
 switch (process.argv[2]) {
@@ -10,10 +11,12 @@ switch (process.argv[2]) {
         spotifySongData();
         break;
     case "movie-this":
+        movieData();
         break;
     case "do-what-it-says":
         break;
     default:
+        console.log("Unsupported command!");
         break;
 }
 
---
+++ omdb.js
@@ -0,0 +1,31 @@
+var request = require("request");
+
+function processOMDB() {
+    let movie = "Mr. Nobody";
+    if (process.argv[3] != null) {
+        movie = process.argv[3];
+    } 
+
+    let queryUrl = "http://www.omdbapi.com/?apikey=trilogy&t=" + movie;
+    request(queryUrl, (err, response, body) => {
+        if(!err && response.statusCode === 200) {
+           let movieData = JSON.parse(body);
+           console.log("***************************************************************************");
+           console.log("Movie Data:");
+           console.log("Title: " + movieData.Title);
+           console.log("Tear: " + movieData.Year);
+           console.log("IMDB Rating: " + movieData.Ratings[0].Value);
+           console.log("Rotten Tomatoes Rating: " + movieData.Ratings[1].Value);
+           console.log("Country produced: " + movieData.Country);
+           console.log("Language(s): " + movieData.Language);
+           console.log("Plot: " + movieData.Plot);
+           console.log("Actors: " + movieData.Actors);
+           console.log("***************************************************************************");
+        } else {
+            
+            console.log(err);
+        }
+    });
+}
+
+module.exports = processOMDB;
