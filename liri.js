037db5eb6ff6a3f25645b0b70091b8e9191313fe
Thomas Martin <tmartin.dev101010@gmail.com>
2018-10-05 18:23:30 -0600

    Spotify work in progress, but getting data from API call.  Pushing to master so I can work on my big screens.

--- liri.js
+++ liri.js
@@ -1,14 +1,5 @@
-// Code to create the Spotify environment variables
-var dotenv = require("dotenv").config();
-if (dotenv.error) {
-    throw dotenv.error
-}
-
-// Importing the keys for Spotify
-var keys = require("./keys.js");
-console.log(keys.spotify);
-
 var bandVenues = require("./band.js");
+var spotifySongData = require("./spotify.js");
 
 // Switch statement to arse the arguments
 switch (process.argv[2]) {
@@ -16,6 +7,7 @@ switch (process.argv[2]) {
         bandVenues();
         break;
     case "spotify-this-song":
+        spotifySongData();
         break;
     case "movie-this":
         break;
---
+++ spotify.js
@@ -0,0 +1,51 @@
+var Spotify = require('node-spotify-api');
+
+// Code to create the Spotify environment variables
+var dotenv = require("dotenv").config();
+if (dotenv.error) {
+    throw dotenv.error
+}
+
+// Importing the keys for Spotify
+var keys = require("./keys.js");
+// console.log(keys.spotify);
+
+
+
+function processSpotify() {
+    let title = process.argv[3];
+    // console.log(title);
+
+    var spotify = new Spotify(keys.spotify);
+
+    // spotify.search({ type: 'track', query: title }, function (err, data) {
+    //     if (err) {
+    //         return console.log('Error occurred: ' + err);
+    //     }
+    //     var items = data.tracks.items;
+    //     // for (let i = 0; i < items.length; ++i) {
+    //     //     console.log(items[i].name);
+    //     // }
+
+    //     for (let item of items) {
+    //         console.log(item.name);
+    //     }
+    // });
+
+    title = title.replace(/ /g, "%20");
+    console.log(title);
+
+    var queryUrl = "https://api.spotify.com/v1/search?type=track&limit=10&q=" + title;
+
+    spotify
+        // .search({ type: "track", query: title })
+        .request(queryUrl)
+        .then((response) => {
+            console.log(response.tracks.items);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+module.exports = processSpotify;
